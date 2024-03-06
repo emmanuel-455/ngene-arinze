e5f9513c4876e6116fb2f989de76c3af0fdf72a1
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-06 23:53:16 +0100

    update

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,12 +4,15 @@ function About() {
   return (
     <div>
       <h1>About me</h1>
-      <p>Experienced and results-oriented Frontend Developer, having 2 years of experience. Offering a strong attention to details and accuracy, excellent problem-solving skills, and the important ability to work in fast-paced team environment. I am the reliable individual with a determination to meet and exceed all the assigned results </p>
       <div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <p></p>
+        </div>
+        <div><p></p></div>
+        <div><p></p></div>
       </div>
+      <p>Experienced and results-oriented Frontend Developer, having 2 years of experience. Offering a strong attention to details and accuracy, excellent problem-solving skills, and the important ability to work in fast-paced team environment. I am the reliable individual with a determination to meet and exceed all the assigned results </p>
+      
     </div>
   )
 }
