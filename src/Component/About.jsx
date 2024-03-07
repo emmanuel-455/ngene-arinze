a8a4e8f09e473e75c103d4b4f2dfd883ceee240c
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-07 01:45:22 +0100

    update

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,16 +4,16 @@ function About() {
   return (
     <div id="About">
       <h1 className="font-normal text-center mb-20 text-xl">About me</h1>
-      <div className="flex justify-between flex-wrap mb-20 m-auto w-[80%]">
-        <div className="text-center border-s-4 border-arinze-blue pl-16">
+      <div className="flex justify-evenly items-center flex-wrap mb-20 m-auto">
+        <div className="text-center md:border-s-4 py-5 px-7 md:border-arinze-blue md:pl-16">
           <p className="mb-3">Experience</p>
           <p>2+ Years</p>
         </div>
-        <div className="text-center border-s-4 border-arinze-blue pl-16">
+        <div className="text-center md:border-s-4 py-5 px-7 md:border-arinze-blue md:pl-16">
           <p className="mb-3">Projects</p>
           <p>50+ Completed</p>
         </div>
-        <div className="text-center border-s-4 border-arinze-blue pl-16">
+        <div className="text-center md:border-s-4 py-5 px-7 md:border-arinze-blue md:pl-16">
           <p className="mb-3">Clients</p>
           <p>100+ World Wide</p>
         </div>
