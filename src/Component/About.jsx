ea3d87a84a9a7633ac3b4872913a7eeba7572780
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-08 19:08:33 +0100

    Update

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div id="About">
       <h1 className="font-normal text-center mb-20 text-xl">About me</h1>
-      <div className="flex justify-evenly items-center flex-wrap mb-20 m-auto">
+      <div className="flex justify-evenly items-center flex-wrap md:mb-20 mb-12 m-auto">
         <div className="text-center md:border-s-4 my-5 px-9 md:border-arinze-blue md:pl-16">
           <p className="mb-3 text-arinze-blue text-lg">Experience</p>
           <p className="text-xs text-A-writeup">2+ Years</p>
@@ -18,7 +18,7 @@ function About() {
           <p className="text-xs text-A-writeup">100+ World Wide</p>
         </div>
       </div>
-      <p className="w-[] text-center m-auto">Experienced and results-oriented Frontend Developer, having 2 years of experience. Offering a strong attention to details and accuracy, excellent problem-solving skills, and the important ability to work in fast-paced team environment. I am the reliable individual with a determination to meet and exceed all the assigned results </p>
+      <p className="md:w-[65%] p-4 text-[15px] text-A-writeup text-center m-auto">Experienced and results-oriented Frontend Developer, having 2 years of experience. Offering a strong attention to details and accuracy, excellent problem-solving skills, and the important ability to work in fast-paced team environment. I am the reliable individual with a determination to meet and exceed all the assigned results </p>
       
     </div>
   )
