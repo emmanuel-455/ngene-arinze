0a0436a1bcc458808b7dd052a7ddc163be02dd64
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-09 23:19:17 +0100

    update

--- src/Component/Proficiency.jsx
+++ src/Component/Proficiency.jsx
@@ -1,58 +1,68 @@
 import React from 'react'
-import Html from "../asset/html 1.png";
-import CSS from "../asset/css 1.png"
-import Boot from "../asset/bootstrap 1.png"
-import Tailwind from "../asset/icons8-tailwind-css-48 1.png"
-import Reactjs from "../asset/icons8-react-100 1.png"
-import github from "../asset/icons8-github 1.png"
-import typescript from "../asset/icons8-typescript-48 1.png"
-import Redux from "../asset/icons8-redux-48 1.png"
+import Html from "../asset/html.png";
+import CSS from "../asset/css.png"
+import JS from "../asset/JS.png"
+import Boot from "../asset/Bootstrap.png"
+import Tailwind from "../asset/Tailwind.png"
+import Reactjs from "../asset/React.png"
+import github from "../asset/GitHub.png"
+import typescript from "../asset/Tailwind.png"
+import Redux from "../asset/Redux.png"
+import Node from "../asset/node-js.png"
+import Postgres from "../asset/Postgres.png"
 
 function Proficiency() {
   return (
     <div id="Proficiency">
       <h1 className="font-medium text-center mb-20 text-xl">Proficiency</h1>
-      <div>
+      <div className='flex justify-between w-[40%] m-auto'>
         <div>
-          <div>
+          <div className='flex items-center'>
             <img className='w-11' src={Html} alt="" />
             <p>HTML</p>
          </div>
-          <div>
+          <div className='flex items-center'>
             <img className='w-11' src={CSS} alt="" />
             <p>CSS</p>
          </div>
-          <div>
-            <img className='w-11' src={CSS} alt="" />
-            <p>CSS</p>
-         </div>
-          <div>
-            <img className='w-11' src={Boot} alt="" />
+          <div className='flex items-center'>
+            <img className='w-11' src={JS} alt="" />
             <p>JavaScript</p>
          </div>
-          <div>
+          <div className='flex items-center'>
+            <img className='w-11' src={Boot} alt="" />
+            <p>Bootstrap</p>
+         </div>
+          <div className='flex items-center'>
             <img className='w-11' src={Tailwind} alt="" />
-            <p></p>
+            <p>Tailwind CSS</p>
          </div>
-          <div>
+          <div className='flex items-center'>
             <img className='w-11' src={Reactjs} alt="" />
-            <p></p>
+            <p>ReactJS</p>
          </div>
-          <div>
+          <div className='flex items-center'>
             <img className='w-11' src={github} alt="" />
-            <p></p>
+            <p>GitHub</p>
          </div>
-          <div>
+          <div className='flex items-center'>
             <img className='w-11' src={typescript} alt="" />
-            <p></p>
+            <p>TypeScript</p>
          </div>
-          <div>
+          <div className='flex items-center'>
             <img className='w-11' src={Redux} alt="" />
-            <p></p>
+            <p>Redux</p>
          </div>
         </div>
         <div>
-
+          <div className='flex items-center'>
+            <p>NodeJS</p>
+            <img className='w-11' src={Node} alt="" />
+          </div>
+          <div className='flex items-center'>
+            <p>Postgres</p>
+            <img className='w-11' src={Postgres} alt="" />
+          </div>
         </div>
       </div>
     </div>
