12637b5f8e491adcd46fb5e7e80f08269f1eb29f
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-09 22:23:25 +0100

    update

--- src/Component/Proficiency.jsx
+++ src/Component/Proficiency.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import Html from "../asset/html 1.png";
+import CSS from "../asset/css 1.png"
+import Boot from "../asset/bootstrap 1.png"
+import Tailwind from "../asset/icons8-tailwind-css-48 1.png"
+import Reactjs from "../asset/icons8-react-100 1.png"
+import github from "../asset/icons8-github 1.png"
+import typescript from "../asset/icons8-typescript-48 1.png"
+import Redux from "../asset/icons8-redux-48 1.png"
 
 function Proficiency() {
   return (
@@ -7,35 +15,39 @@ function Proficiency() {
       <div>
         <div>
           <div>
-            <img src="" alt="" />
+            <img className='w-11' src={Html} alt="" />
+            <p>HTML</p>
+         </div>
+          <div>
+            <img className='w-11' src={CSS} alt="" />
+            <p>CSS</p>
+         </div>
+          <div>
+            <img className='w-11' src={CSS} alt="" />
+            <p>CSS</p>
+         </div>
+          <div>
+            <img className='w-11' src={Boot} alt="" />
+            <p>JavaScript</p>
+         </div>
+          <div>
+            <img className='w-11' src={Tailwind} alt="" />
             <p></p>
          </div>
           <div>
-            <img src="" alt="" />
+            <img className='w-11' src={Reactjs} alt="" />
             <p></p>
          </div>
           <div>
-            <img src="" alt="" />
+            <img className='w-11' src={github} alt="" />
             <p></p>
          </div>
           <div>
-            <img src="" alt="" />
+            <img className='w-11' src={typescript} alt="" />
             <p></p>
          </div>
           <div>
-            <img src="" alt="" />
-            <p></p>
-         </div>
-          <div>
-            <img src="" alt="" />
-            <p></p>
-         </div>
-          <div>
-            <img src="" alt="" />
-            <p></p>
-         </div>
-          <div>
-            <img src="" alt="" />
+            <img className='w-11' src={Redux} alt="" />
             <p></p>
          </div>
         </div>
