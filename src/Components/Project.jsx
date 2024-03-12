492bcac5e5d98ada7f62a376726e4b37e15862ee
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-12 23:35:32 +0100

    update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -26,13 +26,6 @@ function Project() {
             <p></p>
           </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <div>
-            <h1></h1>
-            <p></p>
-          </div>
-        </div>
       </div>
     </div>
   )
